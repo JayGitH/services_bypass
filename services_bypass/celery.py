c52d998f2e251ea26698ee3f6a9a8e8e4ebca5ec
h2magic <h2magic@126.com>
2022-08-28 20:34:47 +0800

    set celery

---
+++ services_bypass/celery.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+from celery import Celery
+
+project = Path(__file__).parent.resolve().name
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"services_center.settings")
+
+app = Celery(project)
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+app.conf.update(
+    enable_utc=True,
+    timezone="Asia/Shanghai"
+)
+
+app.conf.beat_schedule = {
+    'loop-every': {
+        'task': 'loop_query_task',
+        'schedule': 180,
+        'args': ()
+    },
+}
